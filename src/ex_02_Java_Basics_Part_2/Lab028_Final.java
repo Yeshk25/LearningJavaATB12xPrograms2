27c2672f976a907dd3f47d86ff557e9a773c3f66
Yeswanth <k.yeswanth2011@gmail.com>
2025-05-16 02:04:20 +0530

    final keyword prog

---
+++ src/ex_02_Java_Basics_Part_2/Lab028_Final.java
@@ -0,0 +1,10 @@
+package ex_02_Java_Basics_Part_2;
+
+public class Lab028_Final {
+    public static void main(String[] args) {
+        final int a=10;
+        //a=20; // it wont replace because above we declared with final keyword
+
+        System.out.println(a);
+    }
+}
