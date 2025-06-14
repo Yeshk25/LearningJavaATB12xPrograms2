9d43fb1d1c519b72d015f6cca65f40389636ce2c
Yeswanth <k.yeswanth2011@gmail.com>
2025-06-14 14:00:26 +0530

    Switch2

--- src/ex_09_Switch/Lab084_Switch_Print_Day.java
+++ src/ex_09_Switch/Lab084_Switch_Print_Day.java
@@ -4,35 +4,41 @@ import java.util.Scanner;
 
 public class Lab084_Switch_Print_Day {
     public static void main(String[] args) {
-        Scanner s= new Scanner(System.in);
+        Scanner s = new Scanner(System.in);
         System.out.println(" Enter the day to  1 to 7");
-        int day=s.nextInt();
+        //int day=s.nextInt();
+        if (s.hasNextInt()) {
+            int day = s.nextInt();
 
-        switch (day){
-            case 1:
-                System.out.println("mon");
-                break;
-            case 2:
-                System.out.println("tue");
-                break;
-            case 3:
-                System.out.println("Wed");
-                break;
-            case 4:
-                System.out.println("thur");
-                break;
-            case 5:
-                System.out.println("fri");
-                break;
-            case 6:
-                System.out.println("sat");
-                break;
-            case 7:
-                System.out.println("sun");
-                break;
-            default:
-                System.out.println("Please enter valid day number");
 
+            switch (day) {
+                case 1:
+                    System.out.println("mon");
+                    break;
+                case 2:
+                    System.out.println("tue");
+                    break;
+                case 3:
+                    System.out.println("Wed");
+                    break;
+                case 4:
+                    System.out.println("thur");
+                    break;
+                case 5:
+                    System.out.println("fri");
+                    break;
+                case 6:
+                    System.out.println("sat");
+                    break;
+                case 7:
+                    System.out.println("sun");
+                    break;
+                default:
+                    System.out.println("Please enter valid day number");
+
+            }
+        } else {
+            System.out.println("you entered non int values");
         }
     }
 }
---
+++ src/ex_09_Switch/Lab089_IQ.java
@@ -0,0 +1,17 @@
+package ex_09_Switch;
+
+public class Lab089_IQ {
+    public static void main(String[] args) {
+        char ch='A';
+        switch(ch){
+            case 65:
+                System.out.println("match Ascii");
+                break;
+
+            default:
+                System.out.println("No match");
+    }
+
+
+    }
+}
